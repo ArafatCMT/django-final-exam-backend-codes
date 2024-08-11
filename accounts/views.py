48a2300745aa4aec4742fad0f83de2367f452bca
ArafatCMT <arafatemon6969@gmail.com>
2024-08-11 23:39:39 +0600

    verify link add

--- accounts/views.py
+++ accounts/views.py
@@ -33,7 +33,7 @@ class RegistrationView(APIView):
             token = default_token_generator.make_token(user)
             uid = urlsafe_base64_encode(force_bytes(user.pk))
 
-            verification_link = f"http://127.0.0.1:8000/accounts/verify/{uid}/{token}"
+            verification_link = f"https://net-book.onrender.com/accounts/verify/{uid}/{token}"
 
             email_subject = "Verify Your Account"
             email_body = render_to_string('verification_mail.html', {'verification_link': verification_link})
