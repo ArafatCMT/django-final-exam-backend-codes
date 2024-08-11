9b82371e1b18b0a7ce093c25201e7a154fb05b55
ArafatCMT <arafatemon6969@gmail.com>
2024-08-11 09:34:35 +0600

    all changes done

--- likes/serializers.py
+++ likes/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from likes.models import Like
 class LikeSerializer(serializers.ModelSerializer):
     # post = serializers.StringRelatedField(many=False)
-    account = serializers.StringRelatedField(many=False)
+    # account = serializers.StringRelatedField(many=False)
     class Meta:
         model = Like
         fields = ['post','account']
